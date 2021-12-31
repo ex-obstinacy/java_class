2fe7a21eb748d4943910b622e736f479fbb24d6a
itwill <itwill@DESKTOP-ECD8JQR>
2021-12-31 10:15:26 +0900

    첫 커밋 - 2021.12.31

---
+++ JAVA_CLASS/src/ch01/exam01/beginner/Ex01_println.java
@@ -0,0 +1,15 @@
+package ch01.exam01.beginner;
+
+public class Ex01_println {
+
+	public static void main(String[] args) {
+
+		// System.out.println() 메서드 : 괄호 안의 내용을 화면에 출력
+		System.out.println("Hello, World!");
+		
+		System.out.println("박장범");
+		System.out.println(7);
+		
+	}
+
+}
